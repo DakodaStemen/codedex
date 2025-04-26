25011ecd00c7048b652d32763665e89658898b4d
DakodaStemen <dakoda.stemen@proton.me>
2025-04-26 02:04:33 -0700

    Adding a quick lesson from Codedex.

---
+++ python/books_list.py
@@ -0,0 +1,18 @@
+books = [
+"Harry Potter",
+"1984",
+"The Fault in Our Stars",
+"The Mom Test",
+"Life in Code",
+]
+
+books.append("Pachinko")
+print(f"Here is your list: \n{books}")
+
+books.remove("1984")
+print(f"\n\nUpdated list: \n{books}")
+
+books.pop(1)
+print(f"\n\nUpdated list: \n{books}")
+
+
